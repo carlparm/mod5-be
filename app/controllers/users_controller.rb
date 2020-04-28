4189acc0cab1ab244e0996e2b5c1609b57447c92
Carl <carlparm@gmail.com>
2020-04-28 15:36:30 -0400

    user find/create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,14 @@
 class UsersController < ApplicationController
     
     def create
-        user = User.find_by(spotify_id: params[:spotify_id])
+        user = User.find_by(spotify_id: params[:user][:spotify_id])
         if user
             render json: user.to_json()
         else
-            user = User.new(name: params[:name], spotify_id: params[:spotify_id])
+            user = User.new(name: params[:user][:name], spotify_id: params[:user][:spotify_id])
             user.save
             render json: user.to_json()
         end
-    end
-
-
-
-    private
-    def user_params
-        params.require(:user).permit(:name, :github_profile, :cohort_name, :cohort_start, :username, :password)
     end
 
 end
